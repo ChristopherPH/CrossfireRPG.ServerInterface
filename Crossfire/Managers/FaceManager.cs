f1e8adf0423556892f743b2ed6e498dc0feca7c5
Chris <chris@theblackroom.ca>
2021-10-21 16:21:19 -0700

    improve(Faces): Add actions to facemanager to run custom callbacks when a requested face arrives

--- Crossfire/Managers/FaceManager.cs
+++ Crossfire/Managers/FaceManager.cs
@@ -28,38 +28,49 @@ namespace Crossfire.Managers
         static Logger _Logger = new Logger(nameof(FaceManager));
 
         private Dictionary<UInt32, FaceInfo> _Faces { get; } = new Dictionary<UInt32, FaceInfo>();
+        private List<UInt32> _MissingFaces = new List<UInt32>();
+        private List<KeyValuePair<UInt32, Action<Image>>> _FaceAvailableActions = new List<KeyValuePair<uint, Action<Image>>>();
 
         public event EventHandler<FaceAvailableEventArgs> FaceAvailable;
 
-        public Image GetFace(Int32 Face, bool RequestIfMissing = true)
+        public Image GetFace(Int32 Face, Action<Image> FaceAvailable = null, bool RequestIfMissing = true)
         {
-            return GetFace((UInt32)Face, RequestIfMissing);
+            return GetFace((UInt32)Face, FaceAvailable, RequestIfMissing);
         }
 
-        public Image GetFace(Int64 Face, bool RequestIfMissing = true)
+        public Image GetFace(Int64 Face, Action<Image> FaceAvailable = null, bool RequestIfMissing = true)
         {
-            return GetFace((UInt32)Face, RequestIfMissing);
+            return GetFace((UInt32)Face, FaceAvailable, RequestIfMissing);
         }
 
-        public Image GetFace(string Face, bool RequestIfMissing = true)
+        public Image GetFace(string Face, Action<Image> FaceAvailable = null, bool RequestIfMissing = true)
         {
             if (!uint.TryParse(Face, out var value))
                 return null;
 
-            return GetFace(value, RequestIfMissing);
+            return GetFace(value, FaceAvailable, RequestIfMissing);
         }
 
-        public Image GetFace(UInt32 Face, bool RequestIfMissing = true)
+        public Image GetFace(UInt32 Face, Action<Image> FaceAvailable = null, bool RequestIfMissing = true)
         {
             if (Face == 0)
                 return null;
 
             if (!_Faces.TryGetValue(Face, out var faceInfo))
             {
-                if (RequestIfMissing)
+                if ((_Connection.ConnectionStatus == ConnectionStatuses.Connected) &&
+                    RequestIfMissing &&
+                    !_MissingFaces.Contains(Face))
                 {
-                    _Logger.Warning("Map missing face {0}, requesting face", Face);
+                    _MissingFaces.Add(Face);
+
+                    _Logger.Warning("Missing face {0}, requesting face", Face);
                     _Builder.SendAskFace((int)Face);
+                }
+
+                if (FaceAvailable != null)
+                {
+                    _FaceAvailableActions.Add(new KeyValuePair<uint, Action<Image>>(Face, FaceAvailable));
                 }
 
                 return null;
@@ -68,13 +79,39 @@ namespace Crossfire.Managers
             return faceInfo.Image;
         }
 
+        /// <summary>
+        /// Sets the face immediately if available, or sets the face when face is received
+        /// </summary>
+        public void SetFace(UInt32 Face, Action<Image> FaceAvailable)
+        {
+            if ((Face == 0) || (FaceAvailable == null))
+                return;
+
+            var Image = GetFace(Face, FaceAvailable, true);
+            if (Image != null)
+                FaceAvailable(Image);
+        }
+
+        public void SetFace(Int32 Face, Action<Image> FaceAvailable)
+        {
+            SetFace((UInt32)Face, FaceAvailable);
+        }
+
         private void _Connection_OnStatusChanged(object sender, ConnectionStatusEventArgs e)
         {
             _Faces.Clear();
+            _MissingFaces.Clear();
+            _FaceAvailableActions.Clear();
         }
 
         private void _Parser_Image2(object sender, MessageParserBase.Image2EventArgs e)
         {
+            _Logger.Info("Received Face {0}:{1}", e.ImageFace, e.ImageFaceSet);
+
+            var ix = _MissingFaces.IndexOf(e.ImageFace);
+            if (ix != -1)
+                _MissingFaces.RemoveAt(ix);
+
             using (var ms = new MemoryStream(e.ImageData))
             {
                 Image image = Image.FromStream(ms);
@@ -100,6 +137,10 @@ namespace Crossfire.Managers
                 {
                     Face = e.ImageFace
                 });
+
+                foreach (var faceAction in _FaceAvailableActions.Where(x => x.Key == e.ImageFace))
+                    faceAction.Value(image);
+                _FaceAvailableActions.RemoveAll(x => x.Key == e.ImageFace);
             }
         }
 
