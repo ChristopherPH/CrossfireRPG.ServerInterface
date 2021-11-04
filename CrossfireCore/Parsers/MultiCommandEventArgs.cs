aa4440a7bd03d76964a286203e6270bd769d45c2
Chris <chris@theblackroom.ca>
2021-11-04 07:16:34 -0700

    refactor(Core): Move parsers to their own folder

---
+++ CrossfireCore/Parsers/MultiCommandEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossfireCore.ServerInterface
+{
+    public class MultiCommandEventArgs : SingleCommandEventArgs
+    {
+        [Flags]
+        public enum CommandTypes
+        {
+            FirstCommand = 0x01,
+            LastCommand = 0x02,
+            SingleCommand = FirstCommand | LastCommand
+        }
+
+        public CommandTypes CommandType { get; set; }
+        public bool IsFirst => CommandType.HasFlag(CommandTypes.FirstCommand);
+        public bool IsLast => CommandType.HasFlag(CommandTypes.LastCommand);
+        public bool IsSingle => CommandType.HasFlag(CommandTypes.SingleCommand);
+    }
+}
---
+++ CrossfireCore/Parsers/SingleCommandEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossfireCore.ServerInterface
+{
+    public class SingleCommandEventArgs : EventArgs
+    {
+    }
+}
