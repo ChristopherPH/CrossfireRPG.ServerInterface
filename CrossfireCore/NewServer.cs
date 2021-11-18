0dbdbc5936c94b55e6e4f6789c3c6f1f1e255d03
Chris <chris@theblackroom.ca>
2021-11-18 09:51:04 -0800

    fix(gwMap): Use MaxHeadOffset instead of hardcoded 1 to draw offscreen arches

---
+++ CrossfireCore/NewServer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossfireCore
+{
+    public static class NewServer
+    {
+        public const int MaxHeadOffset = 8;
+    }
+}
