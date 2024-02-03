e21e23e83820b927fc2b9cd33834062cae1a7f69
ChristopherPH <ChristopherPH@theblackroom.ca>
2024-02-03 15:12:01 -0800

    feat(Core): Implement INotifyPropertyChanged on DataObject, add BeginPropertiesChanged()/EndPropertiesChanged() to capture multiple properties changed

--- CrossfireCore/Core/Managers/DataObject.cs
+++ CrossfireCore/Core/Managers/DataObject.cs
@@ -1,9 +1,85 @@
-﻿namespace CrossfireCore.Managers
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CrossfireCore.Managers
 {
     /// <summary>
     /// Base class of objects that a DataObjectManager can manage
     /// </summary>
-    public abstract class DataObject
+    public abstract class DataObject : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Sets a property value, and raises a PropertyChanged event if the property changes
+        /// </summary>
+        /// <typeparam name="T">Type of property</typeparam>
+        /// <param name="property">Property to change</param>
+        /// <param name="value">New property value</param>
+        /// <param name="propertyNames">Name of property and additional properties that reference this</param>
+        /// <returns>true if the property changed, false if not</returns>
+        protected bool SetProperty<T>(ref T property, T value, [CallerMemberName] string propertyName = "",
+            params string[] additionalPropertyNames)
+        {
+            //ignore change if the property didn't change
+            if (EqualityComparer<T>.Default.Equals(property, value))
+                return false;
+
+            //set the property
+            property = value;
+
+            //raise event regardless of propertyName
+            OnPropertyChanged(propertyName);
+
+            //save changed properties if capturing all the changed properties
+            if (_propertiesChanged != null)
+                _propertiesChanged.Add(propertyName);
+
+            if (additionalPropertyNames != null)
+            {
+                foreach (var additionalPropertyName in additionalPropertyNames)
+                {
+                    //only raise event and capture properties if a valid propertyName
+                    if (!string.IsNullOrEmpty(additionalPropertyName))
+                    {
+                        OnPropertyChanged(additionalPropertyName); //raise event
+
+                        //save changed properties if capturing all the changed properties
+                        if (_propertiesChanged != null)
+                            _propertiesChanged.Add(additionalPropertyName);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private HashSet<string> _propertiesChanged = null;
+
+        public void BeginPropertiesChanged()
+        {
+            //clear changed properties and start capture
+            _propertiesChanged = new HashSet<string>();
+
+        }
+
+        public IReadOnlyCollection<string> EndPropertiesChanged()
+        {
+            //return null if no properties changed or BeginPropertiesChanged()
+            //was not called
+            if ((_propertiesChanged == null) || (_propertiesChanged.Count == 0))
+                return null;
+
+            //return changed properties after clearing internal list
+            var rc = _propertiesChanged;
+            _propertiesChanged = null;
+            return rc;
+        }
     }
 }
