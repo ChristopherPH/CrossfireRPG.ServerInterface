68f1f45cb8422fb86d8a1f17a2580fa46d862b3e
ChristopherPH <ChristopherPH@theblackroom.ca>
2024-02-19 21:12:31 -0800

    refactor(Core): Split ContainerModifiedEventArgs from ItemManager

--- CrossfireCore/Managers/ItemManagement/ItemManager.cs
+++ CrossfireCore/Managers/ItemManagement/ItemManager.cs
@@ -604,25 +604,6 @@ namespace CrossfireCore.Managers.ItemManagement
         }
     }
 
-    public class ContainerModifiedEventArgs : EventArgs
-    {
-        public enum ModificationTypes
-        {
-            Opened,
-            Closed
-        }
-
-        public ModificationTypes Modification { get; set; }
-        public Item Item { get; set; }
-        public UInt32 ItemTag => Item.Tag;
-        public int ItemIndex { get; set; }
-
-        public override string ToString()
-        {
-            return string.Format("ContainerModified[{0}]: {1}", Modification, Item);
-        }
-    }
-
     public enum ItemLocations
     {
         Unknown,
