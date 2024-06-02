0b28f1a618db4b53bb0f25a39b1ff69b67a42a55
ChristopherPH <ChristopherPH@theblackroom.ca>
2024-06-01 17:22:16 -0700

    refactor(Core): Add comments to SendLookAt() and rename variables for clarity

--- CrossfireCore/Messages/Map/MessageBuilder_Map.cs
+++ CrossfireCore/Messages/Map/MessageBuilder_Map.cs
@@ -14,11 +14,17 @@ namespace CrossfireCore.ServerInterface
             }
         }
 
-        public bool SendLookAt(int x, int y)
+        /// <summary>
+        /// Sends the lookat command to the server
+        /// </summary>
+        /// <param name="xoffset">Tile offset relative to player - player is at tile 0,0</param>
+        /// <param name="yoffset">Tile Offset relative to player - player is at tile 0,0</param>
+        /// <returns>true if the message was sent</returns>
+        public bool SendLookAt(int xoffset, int yoffset)
         {
             using (var ba = new BufferAssembler("lookat"))
             {
-                ba.AddString("{0} {1}", x, y);
+                ba.AddString("{0} {1}", xoffset, yoffset);
 
                 return SendMessage(ba);
             }
