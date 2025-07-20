6a5062f5edfe4eb4372b48a34d63c925d6b25528
ChristopherPH <ChristopherPH@theblackroom.ca>
2025-07-20 15:53:28 -0700

    feat(AccountPlayerManagement): Add AccountPlayerManager

---
+++ Managers/AccountPlayerManagement/AccountPlayer.cs
@@ -0,0 +1,110 @@
+﻿/*
+ * Copyright (c) 2025 Christopher Hayes
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+using System;
+
+namespace CrossfireRPG.ServerInterface.Managers.AccountPlayerManagement
+{
+    /// <summary>
+    /// Hols information about a player in an account.
+    /// </summary>
+    public class AccountPlayer : DataObject
+    {
+        /// <summary>
+        /// 1 Based index of player
+        /// </summary>
+        public int PlayerNumber
+        {
+            get => _PlayerNumber;
+            set => SetProperty(ref _PlayerNumber, value);
+        }
+        int _PlayerNumber;
+
+        /// <summary>
+        /// Player Level
+        /// </summary>
+        public UInt16 Level
+        {
+            get => _Level;
+            set => SetProperty(ref _Level, value);
+        }
+        UInt16 _Level;
+
+        /// <summary>
+        /// Face Number (image number) of the player.
+        /// </summary>
+        public UInt32 FaceNumber
+        {
+            get => _FaceNumber;
+            set => SetProperty(ref _FaceNumber, value);
+        }
+        UInt32 _FaceNumber;
+
+        /// <summary>
+        /// Player Name
+        /// </summary>
+        public string Name
+        {
+            get => _Name;
+            set => SetProperty(ref _Name, value);
+        }
+        string _Name;
+
+        /// <summary>
+        /// Player Class (currently not populated)
+        /// </summary>
+        public string Class
+        {
+            get => _Class;
+            set => SetProperty(ref _Class, value);
+        }
+        string _Class;
+
+        /// <summary>
+        /// Player Race
+        /// </summary>
+        public string Race
+        {
+            get => _Race;
+            set => SetProperty(ref _Race, value);
+        }
+        string _Race;
+
+        /// <summary>
+        /// Face string for the player (eg "evoker.111")
+        /// </summary>
+        public string Face
+        {
+            get => _Face;
+            set => SetProperty(ref _Face, value);
+        }
+        string _Face;
+
+        /// <summary>
+        /// Party the player is in (if any).
+        /// </summary>
+        public string Party
+        {
+            get => _Party;
+            set => SetProperty(ref _Party, value);
+        }
+        string _Party;
+
+        /// <summary>
+        /// Last Map the player was on.
+        /// </summary>
+        public string Map
+        {
+            get => _Map;
+            set => SetProperty(ref _Map, value);
+        }
+        string _Map;
+
+        public override string ToString() =>
+            $"{Name}:{Level} {Class}:{Race} {Map}";
+    }
+}
---
+++ Managers/AccountPlayerManagement/AccountPlayerManager.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * Copyright (c) 2025 Christopher Hayes
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+using CrossfireRPG.ServerInterface.Network;
+using CrossfireRPG.ServerInterface.Protocol;
+using System;
+using System.Collections.Generic;
+
+namespace CrossfireRPG.ServerInterface.Managers.AccountPlayerManagement
+{
+    public class AccountPlayerManager : DataListManager<int, AccountPlayer, List<AccountPlayer>>
+    {
+        public AccountPlayerManager(SocketConnection Connection, MessageBuilder Builder, MessageHandler Handler)
+            : base(Connection, Builder, Handler)
+        {
+            Handler.AccountPlayer += Handler_AccountPlayer;
+        }
+
+        protected override bool ClearDataOnConnectionDisconnect => true;
+        protected override bool ClearDataOnNewPlayer => false;
+        public override DataModificationTypes SupportedModificationTypes =>
+            base.SupportedModificationTypes | DataModificationTypes.Added |
+            DataModificationTypes.MultiCommandStart | DataModificationTypes.MultiCommandEnd;
+
+        /// <summary>
+        /// Number of players associated with the account.
+        /// </summary>
+        private int accountPlayerCount = -1;
+
+        /// <summary>
+        /// Raised when the account players have been populated.
+        /// </summary>
+        public event EventHandler<EventArgs> AccountPlayersPopulated;
+
+        /// <summary>
+        /// Flag indicating whether all account players have been populated.
+        /// </summary>
+        public bool HasAccountPlayers => accountPlayerCount == this.DataObjectCount;
+
+        protected override void ClearData(bool disconnected)
+        {
+            base.ClearData(disconnected);
+
+            accountPlayerCount = -1;
+        }
+
+        private void Handler_AccountPlayer(object sender, MessageHandler.AccountPlayerEventArgs e)
+        {
+            //PlayerNumber 0 indicates that the player list is starting to be populated.
+            if (e.PlayerNumber == 0)
+            {
+                ClearDataObjects();
+
+                accountPlayerCount = e.PlayerCount;
+
+                if (accountPlayerCount == 0)
+                    AccountPlayersPopulated?.Invoke(this, EventArgs.Empty);
+            }
+            else //Add player information
+            {
+                if (e.PlayerNumber == 1)
+                    StartMultiCommand();
+
+                AddDataObject(e.PlayerNumber, new AccountPlayer()
+                {
+                    PlayerNumber = e.PlayerNumber,
+                    Level = e.Level,
+                    FaceNumber = e.FaceNumber,
+                    Name = e.Name,
+                    Class = e.Class,
+                    Race = e.Race,
+                    Face = e.Face,
+                    Party = e.Party,
+                    Map = e.Map,
+                });
+
+                if (e.PlayerNumber == accountPlayerCount)
+                {
+                    EndMultiCommand();
+
+                    AccountPlayersPopulated?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+    }
+}
